b3d591a7c3e49bb8fbaeec3e2b97e022e604c96b
huzaifsk <huzaifsk12@gmail.com>
2022-11-30 12:35:26 +0530

    created my first express server with the help og noden and npm

---
+++ my-express-server/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/",function (request, response){
+    response.send("<h1>Hello world</h1>");
+})
+app.listen(3000, function (){
+    console.log("server started successfully");
+});
