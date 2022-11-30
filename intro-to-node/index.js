beec8be81d87152d4ebc66f2d8eebcd9b45a309e
huzaif shaikh <59112665+huzaifsk@users.noreply.github.com>
2022-11-30 12:32:11 +0530

    created a project that generates random super heroes and super villian name

---
+++ intro-to-node/index.js
@@ -0,0 +1,6 @@
+var superHeroes = require("superheroes");
+var superVillians = require("supervillains");
+var mySuperHeroeName = superHeroes.random();
+var mySuperVillianName = superVillians.random();
+console.log(mySuperHeroeName);
+console.log(mySuperVillianName);
